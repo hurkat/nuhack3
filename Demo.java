21a059aa1a6aacfa8daaa08805b27befeda14624
Manish <abc@d.com>
2020-05-28 17:43:29 +0530

    Initial Commit

---
+++ Demo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Demo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1. Add");
+		System.out.println("2. Subtract");
+		System.out.println("3. Multiply");
+		System.out.println("4. Divide");
+		System.out.print("Enter Choice : ");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+			//
+			break;
+		case 2:
+			//
+			break;
+		case 3:
+			//
+			break;
+		case 4:
+			//
+			break;
+		default:
+			System.out.println("Invalid Choice");
+		}
+	}
+}
